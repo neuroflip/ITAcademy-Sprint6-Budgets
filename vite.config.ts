eea2706e25f2039f845fa93f6f1799d8a3d28b4c
neuroflip <jaanguita@gmail.com>
2025-12-07 22:45:47 +0100

    test: Setup tests and test for App.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ITAcademy-Sprint6-Budgets/",
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setupTests.ts',
+  }
 })
