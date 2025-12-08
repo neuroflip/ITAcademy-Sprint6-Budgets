47efcc771afb77349e35eeef4a2119be5d88220b
neuroflip <jaanguita@gmail.com>
2025-12-08 17:04:09 +0100

    fix: restored vite config to test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: ".",
+  base: "/ITAcademy-Sprint6-Budgets/",
   plugins: [react(), tailwindcss()],
   test: {
     css: true,
